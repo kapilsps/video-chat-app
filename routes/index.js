f7ae44313db3704ef50fd32c00c1c2d53c92aae6
KapilKumar88 <kapil.saini6239@gmail.com>
2021-03-15 15:05:57 +0530

    --

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { v4: uuidV4 } = require('uuid');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.redirect(`/${uuidV4()}`);
+});
+
+
+router.get('/:room', (req, res, next) => {
+  res.render('room', {
+    roomId: req.params.room
+  });
 });
 
 module.exports = router;
